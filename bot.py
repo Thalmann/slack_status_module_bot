11b1ebdffb4165ea89bc1a3c7d7303443f4b2d5f
bt <bt@intempus.dk>
2019-01-29 18:37:49 +0100

    While loop with modules added

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import simplejson
 from httplib2 import Http, ServerNotFoundError
 
@@ -28,4 +29,34 @@ def post_to_slack_channel(channel_webhook_url, message):
     return response, content
 
 
-post_to_slack_channel(test_channel_webhook_url, 'hey')
+class Module():
+
+    def __init__(self, recurring_in_seconds, action, kwargs):
+        self.recurring_in_seconds = recurring_in_seconds
+        self.action = action
+        self.kwargs = kwargs
+        self.last_run = datetime.min
+
+    def run(self):
+        self.action(**self.kwargs)
+        self.last_run = datetime.now()
+
+    def should_run(self):
+        since_last_run = datetime.now() - module.last_run
+        return since_last_run.total_seconds() > self.recurring_in_seconds
+
+
+kwargs = dict(
+    channel_webhook_url=test_channel_webhook_url,
+    message='hey',
+)
+hey_module = Module(
+    recurring_in_seconds=10, action=post_to_slack_channel, kwargs=kwargs)
+modules = [hey_module]
+try:
+    while True:
+        for module in modules:
+            if module.should_run():
+                module.run()
+except Exception as error:
+    print('Failed due to: ', error)
